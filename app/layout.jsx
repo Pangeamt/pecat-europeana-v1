ad798701712819964cd9793fd866130f54f012d5
Ernesto Miguel Rodríguez Rodríguez <ernestomr87@gmail.com>
2024-09-30 12:28:27 +0200

    Update

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,7 +9,15 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "PECAT EUROPEANA",
-  description: "Lenguistic Tools for Europeana",
+  description: "Linguistic Tools for Europeana",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+    android: "/android-chrome-192x192.png",
+    androidLarge: "/android-chrome-512x512.png",
+    favicon: "/favicon-32x32.png",
+  },
 };
 
 export default function RootLayout({ children }) {
